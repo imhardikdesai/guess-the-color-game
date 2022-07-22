396a4b917cee643314fd34da5a55cb49f620a99e
Hardik Desai <hp676913@gmail.com>
2022-07-22 10:51:15 +0530

    fixing display element

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let box = document.getElementsByClassName('box');
 
 let arr = ["b1", "b2", "b3", "b4", "b5", "b6"];
 
+//Genrate Random color
 function randomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -18,29 +19,34 @@ function randomColor() {
     return color;
 }
 
+//Set Random Color in Box
 b1.style.backgroundColor = `${randomColor()}`;
 b2.style.backgroundColor = `${randomColor()}`;
 b3.style.backgroundColor = `${randomColor()}`;
 b4.style.backgroundColor = `${randomColor()}`;
 b5.style.backgroundColor = `${randomColor()}`;
 b6.style.backgroundColor = `${randomColor()}`;
-let rgbText = arr[Math.floor(Math.random() * arr.length)];
+
+//Set RGB Value in Indicator
+
+let rgbText = arr[Math.floor(Math.random() * arr.length)];  // Select Ranom box from array
 console.log(rgbText);
-rgb.innerText = (document.getElementById(`${rgbText}`).style.backgroundColor).toUpperCase();
+rgb.innerText = (document.getElementById(`${rgbText}`).style.backgroundColor).toUpperCase();  // Show RGB Value of that random box
+
 
 function checkColor(myId) {
+
     if (myId == rgbText) {
         document.getElementById('win').style.display = 'block';
+        document.getElementById('lose').style.display = 'none';
+        // box.removeEventListener('click', checkColor);
         for (let i = 0; i < box.length; i++) {
             box[i].style.backgroundColor = `${document.getElementById(`${rgbText}`).style.backgroundColor}`;
         }
-        function checkColor() {
-
-        }
-        checkColor();
     }
     else {
         document.getElementById(`${myId}`).style.display = 'none';
         document.getElementById('lose').style.display = 'block';
+        document.getElementById('win').style.display = 'none';
     }
 }
